c45e178449007ebdc8cee159012eb8caefb7d485
Benjamin Chollet <benjamin.chollet.pro@gmail.com>
2020-09-04 14:15:08 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,197 @@
+const users = [
+  {
+    id: 1,
+    firstname: 'John',
+    lastname: 'Doe',
+    role: 'user',
+    isActive: true,
+    tags: ['tag1', 'tag2', 'tag3'],
+    products: [
+      {
+        id: 12,
+        quantity: 2,
+        price: 12,
+      },
+    ],
+  },
+  {
+    id: 2,
+    firstname: 'John 2',
+    lastname: 'Doe 2',
+    role: 'admin',
+    isActive: true,
+    tags: ['tag1', 'tag2'],
+    products: [
+      {
+        id: 2,
+        quantity: 8,
+        price: 6,
+      },
+      {
+        id: 21,
+        quantity: 3,
+        price: 5,
+      },
+    ],
+  },
+  {
+    id: 3,
+    firstname: 'John 3',
+    lastname: 'Doe 3',
+    role: 'eco',
+    isActive: false,
+    tags: ['tag3'],
+    products: [
+      {
+        id: 7,
+        quantity: 8,
+        price: 3,
+      },
+      {
+        id: 9,
+        quantity: 2,
+        price: 23,
+      },
+    ],
+  },
+  {
+    id: 4,
+    firstname: 'John 4',
+    lastname: 'Doe 4',
+    role: 'user',
+    isActive: true,
+    tags: [],
+    products: [
+      {
+        id: 12,
+        quantity: 2,
+        price: 12,
+      },
+      {
+        id: 8,
+        quantity: 1,
+        price: 10,
+      },
+    ],
+  },
+  {
+    id: 5,
+    firstname: 'John 5',
+    lastname: 'Doe 5',
+    role: 'user',
+    isActive: false,
+    tags: ['tag2', 'tag3'],
+    products: [
+      {
+        id: 12,
+        quantity: 2,
+        price: 12,
+      },
+      {
+        id: 8,
+        quantity: 1,
+        price: 10,
+      },
+    ],
+  },
+  {
+    id: 6,
+    firstname: 'John 6',
+    lastname: 'Doe 6',
+    role: 'admin',
+    isActive: true,
+    tags: ['tag2'],
+    products: [
+      {
+        id: 12,
+        quantity: 2,
+        price: 12,
+      },
+      {
+        id: 8,
+        quantity: 1,
+        price: 10,
+      },
+    ],
+  },
+  {
+    id: 7,
+    firstname: 'John 7',
+    lastname: 'Doe 7',
+    role: 'user',
+    isActive: true,
+    tags: ['tag1', 'tag2'],
+    products: [
+      {
+        id: 12,
+        quantity: 2,
+        price: 12,
+      },
+      {
+        id: 8,
+        quantity: 1,
+        price: 10,
+      },
+    ],
+  },
+  {
+    id: 8,
+    firstname: 'John 8',
+    lastname: 'Doe 8',
+    role: 'admin',
+    isActive: false,
+    tags: ['tag1', 'tag2', 'tag3', 'tag4'],
+    products: [
+      {
+        id: 12,
+        quantity: 2,
+        price: 12,
+      },
+      {
+        id: 8,
+        quantity: 1,
+        price: 10,
+      },
+    ],
+  },
+  {
+    id: 9,
+    firstname: 'John 9',
+    lastname: 'Doe 9',
+    role: 'user',
+    isActive: false,
+    tags: ['tag1', 'tag3'],
+    products: [
+      {
+        id: 9,
+        quantity: 4,
+        price: 21,
+      },
+      {
+        id: 2,
+        quantity: 1,
+        price: 32,
+      },
+    ],
+  },
+  {
+    id: 10,
+    firstname: 'John 10',
+    lastname: 'Doe 10',
+    role: 'eco',
+    isActive: true,
+    tags: ['tag1'],
+    products: [
+      {
+        id: 12,
+        quantity: 2,
+        price: 12,
+      },
+      {
+        id: 8,
+        quantity: 1,
+        price: 10,
+      },
+    ],
+  },
+];
